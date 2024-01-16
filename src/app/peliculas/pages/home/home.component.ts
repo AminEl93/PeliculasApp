9eb848d847e3d6833067a30624bdc3e035a1f392
AminEl93 <amin_11_iverson@hotmail.com>
2024-01-16 13:04:55 +0100

    Scroll infinito de las últimas películas

--- src/app/peliculas/pages/home/home.component.ts
+++ src/app/peliculas/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { PeliculasService } from '../../services/peliculas.service';
 import { Movie } from '../../interfaces/cartelera-response';
 
@@ -12,14 +12,34 @@ export class HomeComponent implements OnInit {
     public movies: Movie[] = [];
     public moviesSlideshow: Movie[] = [];
 
+    // Scroll infinito de las últimas películas
+    @HostListener('window:scroll', ['$event'])    
+    onScroll() {
+        const scroll = (document.documentElement.scrollTop || document.body.scrollTop);
+        const documentHeight = (document.documentElement.clientHeight || document.body.clientHeight);
+        const maxPosition = (document.documentElement.scrollHeight || document.body.scrollHeight);
+        const positionInDocument = (documentHeight + scroll);
+
+        if (positionInDocument === maxPosition) {
+            if (this._peliculasService.cargando) return;  
+            this._peliculasService.getCartelera().subscribe(movies => {
+                this.movies.push(...movies);
+            });
+        }
+
+        /* Alternativa */
+        // const pos = (document.documentElement.scrollTop || document.body.scrollTop) + 1300;
+        // const max = (document.documentElement.scrollHeight || document.body.scrollHeight);
+        // if(pos > max) { mismo código }        
+    }
+
     constructor(private _peliculasService: PeliculasService) { }
 
     ngOnInit(): void {
-        this._peliculasService.getCartelera()
-            .subscribe(movies => {
-                // console.log(resp.results);
-                this.movies = movies;
-                this.moviesSlideshow = movies;
-            })    
+        this._peliculasService.getCartelera().subscribe(movies => {
+            // console.log(resp.results);
+            this.movies = movies;
+            this.moviesSlideshow = movies;
+        })    
     }
 }
--- src/app/peliculas/services/peliculas.service.ts
+++ src/app/peliculas/services/peliculas.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map, tap } from 'rxjs';
+import { Observable, map, of, tap } from 'rxjs';
 
 import { CarteleraResponse, Movie } from '../interfaces/cartelera-response';
 import { environment } from 'src/environments/environment';
@@ -13,9 +13,11 @@ export class PeliculasService {
   
     private baseUrl: string = 'https://api.themoviedb.org/3';
     private carteleraPage = 1;
+    public cargando: boolean = false;
 
     constructor(private _http: HttpClient) { }
 
+    // Parámetros de la URL para obtener la cartelera de películas
     get params() {
         return {
             api_key: environment.api_key,
@@ -26,11 +28,17 @@ export class PeliculasService {
 
     // Obtener la cartelera de todas las películas actuales
     getCartelera(): Observable<Movie[]> {
+        if (this.cargando) return of([]); // Cargando las películas
+        this.cargando = true;
+
         return this._http.get<CarteleraResponse>(`${this.baseUrl}/movie/now_playing`, {
             params: this.params
         }).pipe(
                map( (resp) => resp.results ),
-               tap( () => {this.carteleraPage += 1;} )
+               tap(() => {
+                    this.carteleraPage += 1;
+                    this.cargando = false;
+               })
            );
     }    
 }
