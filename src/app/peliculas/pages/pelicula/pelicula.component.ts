300991ec669a437d4ef246900fabee5ac4e359d3
AminEl93 <amin_11_iverson@hotmail.com>
2024-01-22 11:46:21 +0100

    Optimizar las llamadas al servicio con combineLatest

--- src/app/peliculas/pages/pelicula/pelicula.component.ts
+++ src/app/peliculas/pages/pelicula/pelicula.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { combineLatest } from 'rxjs';
 
 import { PeliculasService } from '../../services/peliculas.service';
 import { NavigationService } from '../../services/navigation.service';
 import { MovieResponse } from '../../interfaces/movie-response';
 import { Cast } from '../../interfaces/credits-response';
+
 @Component({
     selector: 'app-pelicula',
     templateUrl: './pelicula.component.html',
@@ -27,14 +29,15 @@ export class PeliculaComponent implements OnInit {
         // Obtener el ID de una película que viene por la URL
         const { id } = this._activatedRoute.snapshot.params;
 
-        this._peliculasService.getPeliculaDetalle(id).subscribe(movie => {            
-            movie && movie.id === +id
-                ? (this.pelicula = movie)
-                : this._router.navigateByUrl('/home');
-        });
-
-        this._peliculasService.getCast(id).subscribe(cast => {
-            this.cast = cast.filter(actor => actor.profile_path !== null);
+        // Combinar las 2 llamadas al servicio para optimizar el código
+        combineLatest([
+            this._peliculasService.getPeliculaDetalle(id),
+            this._peliculasService.getCast(id)
+        ]).subscribe( ([pelicula, cast]) => {
+                pelicula && pelicula.id === +id
+                    ? (this.pelicula = pelicula)
+                    : this._router.navigateByUrl('/home');
+                this.cast = cast.filter(actor => actor.profile_path !== null);
         });
     }
     
